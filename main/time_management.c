41a7ff6d7ba2d729d95768942dbb9f5e2ec79b65
Deniz Etkar <denizetkar@gmail.com>
2021-05-08 14:33:15 +0200

    ignore sdkconfig to avoid leaking credentials; time must be smoothly synchronized

--- main/time_management.c
+++ main/time_management.c
@@ -10,24 +10,28 @@ void time_sync_notification_cb(struct timeval *tv)
     ESP_LOGI(TAG, "Notification of a time synchronization event");
 }
 
-static void initialize_sntp(void){
-	sntp_setoperatingmode(SNTP_OPMODE_POLL);
-	sntp_setservername(0, SNTP_SERVER);
-	sntp_set_time_sync_notification_cb(time_sync_notification_cb);
-	sntp_init();
+static void initialize_sntp(void)
+{
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, SNTP_SERVER);
+    sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+    sntp_set_sync_mode(SNTP_SYNC_MODE_SMOOTH);
+    sntp_init();
 }
 
 static void obtain_time(void)
 {
     int retry = 0;
     const int retry_count = SNTP_SYNC_RETRY;
-    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count)
+    {
         ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
-    if(retry == retry_count){
-    	ESP_LOGE(TAG, "Could not retrieve time.!\n");
-    	esp_restart();
+    if (retry == retry_count)
+    {
+        ESP_LOGE(TAG, "Could not retrieve time.!\n");
+        esp_restart();
     }
     // Set timezone to MESZ
     setenv("TZ", "UTC-2", 1);
@@ -35,27 +39,31 @@ static void obtain_time(void)
     ESP_LOGI(TAG, "System time set.");
 }
 
-struct tm read_time(){
-	time_t now;
-	struct tm timeinfo;
-	time(&now);
-	localtime_r(&now, &timeinfo);
-	return timeinfo;
+struct tm read_time()
+{
+    time_t now;
+    struct tm timeinfo;
+    time(&now);
+    localtime_r(&now, &timeinfo);
+    return timeinfo;
 }
 
-bool is_time_set(){
-	struct tm timeinfo = read_time();
-	return timeinfo.tm_year >= ESP_EXPECTED_TIME_YEARS;
+bool is_time_set()
+{
+    struct tm timeinfo = read_time();
+    return timeinfo.tm_year >= ESP_EXPECTED_TIME_YEARS;
 }
 
-void setup_sntp(){
-    ESP_ERROR_CHECK(nvs_flash_init() );
+void setup_sntp()
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     // TODO why failing?
     //ESP_ERROR_CHECK( esp_event_loop_create_default() );
 
-	initialize_sntp();
-	if(!is_time_set()){
-		obtain_time();
-	}
+    initialize_sntp();
+    if (!is_time_set())
+    {
+        obtain_time();
+    }
 }
